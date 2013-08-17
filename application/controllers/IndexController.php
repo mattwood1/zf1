b8a252f8b1bec4588312d6e6a488dfbb5a75a2d3
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@a91adf17-b459-06e3-5023-da5c267e61b4>
2013-08-17 02:46:00 +0000

    Added search function. Needs a form.

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,23 +10,25 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $paginator = new Doctrine_Pager(
-            Doctrine_Core::getTable('God_Model_Model')
-                ->createQuery('m')
-                ->innerJoin('m.names n')
-                ->innerJoin('m.photosets p')
+        $query = Doctrine_Core::getTable('God_Model_Model')
+                        ->createQuery('m')
+                        ->innerJoin('m.names n')
+                        ->innerJoin('m.photosets p')
 
-                ->where('m.active = ?', 1)
-                ->andWhere('m.ranking >= ?', 0)
+                        ->where('m.active = ?', 1)
+                        ->andWhere('m.ranking >= ?', 0)
 
-                ->andWhere('n.default = ?', 1)
+                        ->andWhere('n.default = ?', 1)
 
-                ->andWhere('p.active = ?', 1)
-                ->andWhere('p.manual_thumbnail = ?', 1)
-                ->orderBy('m.ranking desc, p.name desc')
+                        ->andWhere('p.active = ?', 1)
+                        ->andWhere('p.manual_thumbnail = ?', 1)
+                        ->orderBy('m.ranking desc, p.name desc');
 
-                ,
-            $this->_getParam('page',1), 24 );
+        if ($this->_request->isPost()) {
+            $query->andWhere('n.name like ?', '%' . $this->_request->getParam('search') . '%') ;
+        }
+
+        $paginator = new Doctrine_Pager($query, $this->_getParam('page',1), 24 );
 
         $models = $paginator->execute();
 
