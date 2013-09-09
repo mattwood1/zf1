2a9bb24a1b7110154d6d89a978477cf37ee7bfe5
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@a91adf17-b459-06e3-5023-da5c267e61b4>
2013-09-09 23:50:35 +0000

    Barebones user controller.

---
+++ application/controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+class UserController extends Coda_Controller
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+
+    }
+
+    // List users
+
+    // Change current password
+
+    // Add a new user
+
+    // manage user access? ACL
+}
