9b1a1338422dcb1a1fd5b5400019c074b924b0ae
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@a91adf17-b459-06e3-5023-da5c267e61b4>
2013-09-18 21:55:41 +0000

    Created a scheduled job to update models photosets

--- jobs/Job/Model/UpdatePhotosets.php
+++ jobs/Job/Model/UpdatePhotosets.php
@@ -8,8 +8,6 @@ class Job_Model_UpdatePhotosets extends Job_Abstract
 {
     public function run()
     {
-        echo 'Update Photosets job entered';
-
         $models = Doctrine_Core::getTable('God_Model_Model')
                     ->findBy('photosetsChecked', '< '.date("Y-m-d", strtotime("-1 day")) );
 
