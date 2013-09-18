5b6e11f9108d9ce9470d18f7b5272bc0592665e7
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@a91adf17-b459-06e3-5023-da5c267e61b4>
2013-09-18 21:54:52 +0000

    Created a scheduled job to update models photosets

--- jobs/Job/Model/UpdatePhotosets.php
+++ jobs/Job/Model/UpdatePhotosets.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * This job is responsible for Updating Models Photosets that are out of date.
+ *
+ * Scheduled to run at 1am every day
  */
 class Job_Model_UpdatePhotosets extends Job_Abstract
 {
