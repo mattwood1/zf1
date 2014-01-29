3d43cc24b23b82157eefee695813d84284d90275
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@a91adf17-b459-06e3-5023-da5c267e61b4>
2014-01-29 21:13:28 +0000

    link model name to url in the model.

--- jobs/Job/WebUrl/LinkModelNames.php
+++ jobs/Job/WebUrl/LinkModelNames.php
@@ -19,28 +19,7 @@ class Job_WebUrl_LinkModelNames extends Job_Abstract
             //var_dump($webUrl->url);
             $webUrl->linked = -2;                        // Processed by this script
 
-            $modelNameTable = new God_Model_ModelNameTable;
-            $modelNames = $modelNameTable->getInstance()->findAll();
-
-            foreach ($modelNames as $modelName) {
-                //var_dump(($modelName->name));
-                $name = str_replace(" ", "[\s\-\_]", $modelName->name);
-
-                if (preg_match("~(" . $name . ")~i", $webUrl->url)) {
-                    $webUrl->linked = -5;                // Name Match found
-                    $modelNamewebUrl = God_Model_ModelNameWebURLTable::getInstance()->createQuery('mnwu')
-                        ->where('model_name_id = ?', $modelName->ID)
-                        ->andWhere('webUrl_id = ?', $webUrl->id)
-                        ->execute();
-                    if (!$modelNamewebUrl->toArray()) {
-                        $modelNamewebUrl = Doctrine_Core::getTable('God_Model_ModelNameWebURL')->create(array(
-                                'model_name_id' => $modelName->ID,
-                                'webUrl_id'     => $webUrl->id
-                        ));
-                        $modelNamewebUrl->save();
-                    }
-                }
-            }
+            $webUrl->linkModelNameToUrl();
 
             $webUrl->save();
 
