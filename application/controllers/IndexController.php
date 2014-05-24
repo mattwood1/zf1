38a7f6d6471e73d41c5e6301a5e45c41433a5a0a
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@a91adf17-b459-06e3-5023-da5c267e61b4>
2014-05-24 00:56:27 +0000

    Updates using bootstrap3

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -23,6 +23,7 @@ class IndexController extends Coda_Controller
         $paginator = new Doctrine_Pager($modelTable->getQuery(), $this->_getParam('page', 1), 18);
         $models = $paginator->execute();
 
+        $this->view->title = 'Goddesses';
         $this->view->paginator = $paginator;
         $this->view->models = $models;
     }
