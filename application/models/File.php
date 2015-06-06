8f8dd4286ee9f50062a51b00eed9d6d51536ee63
Matthew Wood <matthew.john.wood@gmail.com>
2015-06-06 12:55:26 +0100

    Adding a new model for handling files.

---
+++ application/models/File.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * File Handler
+ */
+class God_Model_File
+{
+    protected $_handle;
+    protected $_path;
+    protected $_files = array();
+    protected $_directories = array();
+
+
+    public function __construct($path)
+    {
+        $this->_path = $path;
+        $this->_handle = opendir($path);
+        
+        if ($this->_handle) {
+            while (false !== ($item = readdir($this->_handle))) {
+                if ($item != "." && $item != "..") {      // remove '.' '..' directories
+                    if (is_file($this->_path.'/'.$item) == true) {
+                        $this->_files[] = $item;
+                    }
+                    if (is_dir($this->_path.'/'.$item) == true) {
+                        $this->_directories[] = $item;
+                    }
+                }
+            }
+        }
+        return $this;
+    }
+    
+    public function __destruct()
+    {
+        closedir($this->_handle);
+    }
+
+    public function getDirectories()
+    {
+        return $this->_directories;
+    }
+    
+    public function getFiles()
+    {
+        return $this->_files;
+    }
+}
