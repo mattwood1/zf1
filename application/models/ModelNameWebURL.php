72c455d6d7f1b56aa58f12bde80cfcd2d811e2bd
Matthew Wood <matthew.john.wood@gmail.com>
2015-06-06 12:00:44 +0100

    Moved all databse releated elements into the Base directory.

--- application/models/ModelNameWebURL.php
+++ application/models/ModelNameWebURL.php
@@ -1,43 +1,5 @@
 <?php
-class God_Model_ModelNameWebURL extends Doctrine_Record
+class God_Model_ModelNameWebURL extends God_Model_Base_ModelNameWebURL
 {
-    public function setTableDefinition()
-    {
-        $this->setTableName('model_name_webUrls');
-
-        $this->hasColumn('id', 'integer', 11, array(
-                'type'               => 'integer',
-                'fixed'              => 0,
-                'unsigned'           => true,
-                'primary'            => true,
-                'autoincrement'      => true,
-                'length'             => '11',
-        ));
-
-
-        $this->hasColumn('model_name_id', 'integer', 11, array(
-                'type'               => 'integer',
-                'length'             => '11'
-        ));
-
-        $this->hasColumn('webUrl_id', 'integer', 11, array(
-                'type'               => 'integer',
-                'length'             => '11'
-        ));
-    }
-
-    public function setUp()
-    {
-        $this->hasOne('God_Model_ModelName as modelName', array(
-                'local'   =>  'model_name_id',
-                'foreign' =>  'ID',
-                //'cascade' => array('delete')
-        ));
-
-        $this->hasMany('God_Model_WebURL as webUrl', array(
-                'local'   =>  'webUrl_id',
-                'foreign' =>  'id',
-                //'cascade' => array('delete')
-        ));
-    }
+    
 }
