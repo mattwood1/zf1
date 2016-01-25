f43f793dad199d042f1b24b3e2011bd1fb1b225c
Matthew Wood <matthew.john.wood@gmail.com>
2016-01-25 21:56:25 +0000

    Cleanup script added to remove duplicate links.

--- jobs/Job/WebUrl/LinkModelNames.php
+++ jobs/Job/WebUrl/LinkModelNames.php
@@ -8,6 +8,8 @@ class Job_WebUrl_LinkModelNames extends Job_Abstract
 {
     public function run()
     {
+        $this->cleanup();
+        
         $webUrlsTable = new God_Model_WebURLTable;
         $webUrlsQuery = $webUrlsTable->getInstance()
             ->createQuery('wu')
@@ -23,6 +25,33 @@ class Job_WebUrl_LinkModelNames extends Job_Abstract
 
             $webUrl->save();
 
+        }
+    }
+    
+    protected function cleanup()
+    {
+        $conn = Doctrine_Manager::getInstance()->connection();
+        
+        $duplicates = $conn->execute('
+            SELECT mnwu2.id 
+            FROM `model_name_webUrls` mnwu1 
+            join model_name_webUrls mnwu2 on (
+                mnwu1.model_name_id = mnwu2.model_name_id 
+                AND mnwu1.webUrl_id = mnwu2.webUrl_id 
+                AND mnwu1.id != mnwu2.id
+            )
+            limit 100');
+        $duplicatesResults = $duplicates->fetchAll();
+        
+        if ($duplicatesResults) {
+            $removeIds = array();
+            foreach ($duplicatesResults as $duplicatesResult) {
+                $removeIds[] = $duplicatesResult['id'];
+            }
+                
+            $conn->execute('
+                DELETE FROM `model_name_webUrls` 
+                WHERE `model_name_webUrls`.`id` in (' . implode(',', $removeIds) . ')');
         }
     }
 }
