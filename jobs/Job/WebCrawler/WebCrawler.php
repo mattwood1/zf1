2387b22761ccfe27d937b83ab9a5a0f2edbc6ab2
Matthew Wood <matthew.john.wood@gmail.com>
2017-04-27 00:06:54 +0100

    Better database utilisation using a single query for many urls to check.

--- jobs/Job/WebCrawler/WebCrawler.php
+++ jobs/Job/WebCrawler/WebCrawler.php
@@ -13,23 +13,20 @@ class Job_WebCrawler_WebCrawler extends Job_Abstract
         $webCrawlerQuery = $webCrawlerTable->getInstance()
             ->createQuery('wc')
             ->where('url = ?', '')
-            ->limit(300);
+            ->limit(100);
         $webCrawlerLinks = $webCrawlerQuery->execute();
 
         foreach ($webCrawlerLinks as $webCrawlerLink) {
             checkCPULoad();
 
             $curl = new God_Model_Curl();
-
             $curl->Curl($webCrawlerLink->link, null,false, 10, true);
 
             $webCrawlerLink->statuscode = $curl->statusCode();
             $webCrawlerLink->contenttype = $curl->contentType();
             $webCrawlerLink->contentlength = $curl->contentLength();
             $webCrawlerLink->url = $curl->lastUrl();
-
             $webCrawlerLink->save();
-
         }
 
         // Get Curl contents for urls not followed and later scheduled
@@ -37,17 +34,15 @@ class Job_WebCrawler_WebCrawler extends Job_Abstract
             ->createQuery('wc')
             ->where('(followed = ? or frequency is not null)', 0)
             ->andWhere('contenttype like ?', '%text/html%')
-            ->andWhere('(date < ? or date is null)', date("Y-m-d H:i:s"))
-            ->limit(50);
+            ->andWhere('(date < ? or date is null)', date("Y-m-d H:i:s"));
+//            ->limit(50);
         $webCrawlerUrls = $webCrawlerQuery->execute();
 
         foreach ($webCrawlerUrls as $webCrawlerUrl) {
             checkCPULoad();
 
             $curl = new God_Model_Curl();
-
             $curl->Curl($webCrawlerUrl->url, null, null, 10, true);
-
             $content = $curl->rawdata();
 
 // Should be in the DOMXPath Model, but understanding how to make it a model is tricky
@@ -55,49 +50,59 @@ class Job_WebCrawler_WebCrawler extends Job_Abstract
             $dom = new DOMDocument();
             @$dom->loadHTML($content);
 
+            $links = array();
+
             $linkspath = new DOMXPath($dom);
-            $links = $linkspath->evaluate('//a');
 
-            for ($i = 0; $i < $links->length; $i++) {
-                $link = $links->item($i);
+            $aTag = $linkspath->evaluate('//a');
+            for ($i = 0; $i < $aTag->length; $i++) {
+                $link = $aTag->item($i);
                 $href = $link->getAttribute('href');
-
-                $href = $curl->normalizeURL($href, $webCrawlerUrl->url);
-
-                $dblinkQuery = God_Model_WebCrawlerTable::getInstance()
-                    ->createQuery('wc')
-                    ->where('link = ?', $href)
-                    ->orWhere('url = ?', $href);
-                $dblink = $dblinkQuery->execute(array(), Doctrine_Core::HYDRATE_ARRAY);
-
-                if (count($dblink) == 0) {
-
-                    $newLink = new God_Model_WebCrawler();
-                    $newLink->fromArray(array(
-                        'link' => $href,
-                        'parent' => $webCrawlerUrl->id
-                    ));
-                    $newLink->save();
-
-                }
-
+                $links[] = $curl->normalizeURL($href, $webCrawlerUrl->url);
             }
 
-            $images = $linkspath->evaluate('//img');
-
-            for ($i = 0; $i < $images->length; $i++) {
-                $image = $images->item($i);
+            $imgTag = $linkspath->evaluate('//img');
+            for ($i = 0; $i < $imgTag->length; $i++) {
+                $image = $imgTag->item($i);
                 $src = $image->getAttribute('src');
+                $links[] = $curl->normalizeURL($src, $webCrawlerUrl->url);
+            }
 
-                $src = $curl->normalizeURL($src, $webCrawlerUrl->url);
+            // Known links
+            $knownLinks = array();
+            $dblinkQuery = God_Model_WebCrawlerTable::getInstance()
+                ->createQuery('wc')
+                ->whereIn('link', $links)
+                ->orWhereIn('url', $links);
+            $dblinks = $dblinkQuery->execute(array(), Doctrine_Core::HYDRATE_ARRAY);
 
-                $dblink = God_Model_WebCrawlerTable::getInstance()->findBy('link', $src);
-                if (count($dblink) == 0) {
+            foreach ($dblinks as $dblink) {
+                $knownLinks[] = $dblink['link'];
+                $knownLinks[] = $dblink['url'];
+            }
+            $knownLinks = array_filter($knownLinks);
+            $knownLinks = array_unique($knownLinks);
+
+            $linksMissing = array_diff($links, $knownLinks);
+
+            if ($linksMissing) {
+                foreach ($linksMissing as $linkMissing) {
+
+                    $parseLink = parse_url($linkMissing);
+                    $parseUrl = parse_url($webCrawlerUrl->url);
+
+                    $parent = 0;
+                    if (array_key_exists('host', $parseLink) && array_key_exists('host', $parseUrl) && $parseLink['host'] == $parseUrl['host']) {
+                        $parent = $webCrawlerUrl->id;
+                    }
+                    else {
+                        $parent = $webCrawlerUrl->id;
+                    }
 
                     $newLink = new God_Model_WebCrawler();
                     $newLink->fromArray(array(
-                        'link' => $src,
-                        'parent' => $webCrawlerUrl->id
+                        'link' => $linkMissing,
+                        'parent' => $parent
                     ));
                     $newLink->save();
 
