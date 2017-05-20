532d23c875c594ce2c51c2751a3200b23e3da359
Matthew Wood <matthew.john.wood@gmail.com>
2017-05-20 11:17:00 +0100

    Adding web crawler urls links to model names when being added.

--- application/models/WebCrawlerUrlModelName.php
+++ application/models/WebCrawlerUrlModelName.php
@@ -3,6 +3,31 @@ class God_Model_WebCrawlerUrlModelName extends God_Model_Base_WebCrawlerUrlModel
 {
     public static function createLink(God_Model_WebCrawlerUrl $url)
     {
+        // Only link html pages not other content types
+        if (stripos($url->contexttype, "text/html") === false) return;
 
+        /*
+         * Create a quick array of the model names, processed modelNameID => model_name
+         */
+
+        $modelNameTable = new God_Model_ModelNameTable();
+        $modelNames = $modelNameTable->getActiveModelNames();
+
+        $names = array();
+        foreach ($modelNames as $modelName) {
+            $names[$modelName['ID']] = strtolower(str_replace(" ", "[\s\-\_]", $modelName['name']));
+        }
+        $names = array_filter($names);
+
+        foreach ($names as $modelNameID => $name) {
+            if (preg_match("~((?:[\-\/])" . $name . "(?:[\-\/\_\.])?)~i", $url['url'], $matches)) {
+                _d($modelNameID, $name, $url['url'], $matches);
+
+                $urlModelName = new God_Model_WebCrawlerUrlModelName();
+                $urlModelName->model_name_id = $modelNameID;
+                $urlModelName->webcrawler_url_id = $url->id;
+                $urlModelName->save();
+            }
+        }
     }
 }
