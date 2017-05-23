086bd6734168d83e7ca269855e1141285cb406ba
Matthew Wood <matthew.john.wood@gmail.com>
2017-05-23 23:07:58 +0100

    Update to the links of the web crawler to use a priority.

--- jobs/Job/WebCrawler/Link.php
+++ jobs/Job/WebCrawler/Link.php
@@ -19,6 +19,7 @@ class Job_WebCrawler_Link extends Job_Abstract
             ->leftJoin('wl.parent_link as wll')
             ->where('wl.url_id = ?', 0)
             ->orderBy('wl.priority desc, wll.parent_url_id asc')
+            ->groupBy('wl.id')
             ->limit(500);
 //        _dexit($webCrawlerLinkQuery);
         $webCrawlerLinks = $webCrawlerLinkQuery->execute();
